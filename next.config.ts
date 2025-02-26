46c9c743c444fd8b4e9cd760b4c863a7b6f9150c
fusq <dervieu.octave@gmail.com>
2025-02-26 18:16:02 +0900

    f

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from "next";
-const { withPlausibleProxy } = require('next-plausible')
-
+import { withPlausibleProxy } from 'next-plausible'
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
